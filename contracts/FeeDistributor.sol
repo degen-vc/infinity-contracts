df42b91f1a79a61f85b43afb5073ce9ddcb6418e
Elena Derymova <elenaderymova@gmail.com>
2021-04-23 10:54:53 +0300

    Fee distributor rough updates

--- contracts/FeeDistributor.sol
+++ contracts/FeeDistributor.sol
@@ -1,12 +1,22 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.7.4;
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
 import "./IInfinityProtocol.sol";
 
 contract FeeDistributor is Ownable {
+    using SafeMath for uint;
 
+    struct FeeRecipient {
+        address liquidVault;
+        address secondaryAddress;
+        uint256 liquidVaultShare; //percentage between 0 and 100
+        uint256 burnPercentage;
+    }
+    
     IInfinityProtocol public infinity;
-    address public liquidVault;
+    FeeRecipient public recipients;
+
     bool public initialized;
 
     modifier seeded {
@@ -19,19 +29,50 @@ contract FeeDistributor is Ownable {
 
     function seed(
         address _infinity,
-        address _vault
+        address _vault,
+        address _secondaryAddress,
+        uint _liquidVaultShare,
+        uint _burnPercentage
     ) external onlyOwner {
+        require(
+            _liquidVaultShare.add(_burnPercentage) <= 100,
+            "FeeDistributor: liquidVault + burnPercentage incorrect sets"
+        );
         infinity = IInfinityProtocol(_infinity);
-        liquidVault = _vault;
+        recipients.liquidVault = _vault;
+        recipients.secondaryAddress = _secondaryAddress;
+        recipients.liquidVaultShare = _liquidVaultShare;
+        recipients.burnPercentage = _burnPercentage;
         initialized = true;
     }
 
-    function burn(uint amount) external seeded onlyOwner {
-        infinity.burn(amount);
-    }
-
-    function distributeFees() external seeded onlyOwner {
+    function distributeFees() public seeded {
         uint balance = infinity.balanceOf(address(this));
-        infinity.transfer(liquidVault, balance);
+
+        if (balance < 100) {
+            return;
+        }
+
+        uint liquidShare;
+        uint burningShare;
+
+        if (recipients.liquidVaultShare > 0) {
+            liquidShare = recipients.liquidVaultShare.mul(balance).div(100);
+
+            require(
+                infinity.transfer(recipients.liquidVault, liquidShare),
+                "FeeDistributor: transfer to LiquidVault failed"
+            );
+        }
+
+        if (recipients.burnPercentage > 0) {
+            burningShare = recipients.burnPercentage.mul(balance).div(100);
+            infinity.burn(burningShare);
+        }
+
+        require(
+            infinity.transfer(recipients.secondaryAddress, balance.sub(liquidShare).sub(burningShare)),
+            "FeeDistributor: transfer to the secondary address failed"
+        );
     }
 }
